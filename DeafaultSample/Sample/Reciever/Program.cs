39bb71b0b900c12adbbb791f5d267092976a98af
sunnyday0932 <sunnyday0932@gmail.com>
2021-05-17 20:11:13 +0800

    feat: Deafault Sample。

---
+++ DeafaultSample/Sample/Reciever/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
