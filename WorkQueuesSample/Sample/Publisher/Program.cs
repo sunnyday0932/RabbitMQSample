7482b50364b6c780f0767ecc627d4548d76dc4a8
sunnyday0932 <sunnyday0932@gmail.com>
2021-05-18 21:16:45 +0800

    feat: WorkQueues Sample。

---
+++ WorkQueuesSample/Sample/Publisher/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "a1234"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "WorkeTestQueue",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                //訊息持久化保存設定
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "WorkeTestQueue",
+                                     basicProperties: properties, //加入properties設定
+                                     body: body);
+                Console.WriteLine(" 送出訊息： {0}", message);
+            }
+
+            Console.WriteLine("請按任意鍵離開!");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ WorkQueuesSample/Sample/Reciever2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Reciever2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "a1234"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+
+                channel.QueueDeclare
+                    (
+                        queue: "WorkeTestQueue",
+                        durable: true, //記得Reciever queue設定要跟Publisher一致
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                //平均分配訊息設定
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" 收到訊息： {0}", message);
+
+                    var dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine("任務完成");
+
+                    //設定執行完成回覆Ack
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+                channel.BasicConsume
+                    (
+                        queue: "WorkeTestQueue",
+                        autoAck: false, //取消自動回覆
+                        consumer: consumer
+                    );
+
+                Console.WriteLine("請按任意鍵離開!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
