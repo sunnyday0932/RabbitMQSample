e310b99737795d51941dce3f8452dd355c33e995
sunnyday0932 <sunnyday0932@gmail.com>
2021-05-20 20:10:18 +0800

    feat: Publish/Subscribe Sample。

---
+++ PublishSubscribeSample/Sample/Publisher/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "a1234"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //創建一個Exchange設定為logs
+                channel.ExchangeDeclare(exchange: "LogsExchange", ExchangeType.Fanout);
+
+                var message = GetMessage(args);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "LogsExchange", //設定我們的Exchange為LogsExchange
+                                     routingKey: "",           //不指定Queue
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine(" 送出訊息： {0}", message);
+            }
+
+            Console.WriteLine("請按任意鍵離開!");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0)
+                   ? string.Join(" ", args)
+                   : "info: Hello World!");
+        }
+    }
+}
---
+++ PublishSubscribeSample/Sample/Reciver1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Reciver1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "a1234"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "LogsExchange", ExchangeType.Fanout);
+
+                //這方法會隨機建立一個非持久化、自動刪除的、獨立的Queue
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                                  exchange: "LogsExchange", //指定透過我們的logsExchange
+                                  routingKey: "");
+
+                Console.WriteLine(" 等待拿取log ");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" 收到訊息： {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine("請按任意鍵離開!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
