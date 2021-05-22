6b2ef2768dea0d7fb588b6ac8dd74d9e036ed263
sunnyday0932 <sunnyday0932@gmail.com>
2021-05-22 08:34:06 +0800

    feat: Routing Sample。

---
+++ RoutingSample/Sample/Sample/Publisher/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "a1234"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //創建一個Exchange設定為direct_logs 透過direct方式發送
+                channel.ExchangeDeclare(
+                    exchange: "direct_logs", 
+                    ExchangeType.Direct);
+
+                //我們已接收到的第一個參數來當key決定要丟給哪一個Queue
+                var key = (args.Length > 0) ? args[0] : "info";
+
+                var message = (args.Length > 1)
+                              ? string.Join(" ", args.Skip(1).ToArray())
+                              : "Hello World!";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "direct_logs", //設定我們的Exchange為logs
+                                     routingKey: key,   
+                                     basicProperties: null,
+                                     body: body);
+
+                Console.WriteLine($" 送出訊息： '{key}' : '{message}'");
+            }
+
+            Console.WriteLine("請按任意鍵離開!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RoutingSample/Sample/Sample/Reciver2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Reciver2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "admin",
+                Password = "a1234"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //一樣是direct_logs Exchange
+                channel.ExchangeDeclare(
+                    exchange: "direct_logs",
+                    ExchangeType.Direct);
+
+                //這方法會隨機建立一個非持久化、自動刪除的、獨立的Queue
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName,
+                                  exchange: "direct_logs", //指定透過我們的logsExchange
+                                  routingKey: "warning");     //只拿取Warning的logs
+
+                Console.WriteLine(" 等待拿取log ");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" 收到訊息： {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine("請按任意鍵離開!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
